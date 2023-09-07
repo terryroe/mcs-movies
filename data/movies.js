bffa8d08557b7ee32edef2eef631ab0a9a294dc5
Terry Roe <terryroe@gmail.com>
2023-09-07 15:24:57 -0600

    add data and images

---
+++ data/movies.js
@@ -0,0 +1,91 @@
+export const movies = [
+  {
+    id: 1,
+    Title: 'Silence of the Lambs',
+    Description: 'A classic tale of dinner with friends.',
+    Genre: 'Thriller',
+    Director: 'Jonathan Demme',
+    ImagePath: 'silenceofthelambs.jpg',
+  },
+  {
+    id: 2,
+    Title: 'Pulp Fiction',
+    Description:
+      'The lives of two mob hitmen, a boxer, a gangster and his wife, and a pair of diner bandits intertwine in four tales of violence and redemption.',
+    ImagePath: 'pulpfiction.jpg',
+    Director: 'Quentin Tarantino',
+    Genre: 'Drama',
+  },
+  {
+    id: 3,
+    Title: 'Excalibur',
+    Description:
+      'Merlin the magician helps Arthur Pendragon unite the Britons around the Round Table of Camelot, even as dark forces conspire to tear it apart.',
+    ImagePath: 'excalibur.jpg',
+    Genre: 'Fantasy',
+    Director: 'John Boorman',
+  },
+  {
+    id: 4,
+    Title: 'Casablanca',
+    Description:
+      'A cynical expatriate American cafe owner struggles to decide whether or not to help his former lover and her fugitive husband escape the Nazis in French Morocco.',
+    ImagePath: 'casablanca.jpg',
+    Director: 'Michael Curtiz',
+    Genre: 'Romance',
+  },
+  {
+    id: 5,
+    Title: 'Robocop',
+    Description:
+      'In a dystopic and crime-ridden Detroit, a terminally wounded cop returns to the force as a powerful cyborg haunted by submerged memories.',
+    ImagePath: 'robocop.jpg',
+    Genre: 'Action',
+    Director: 'Paul Verhoeven',
+  },
+  {
+    id: 6,
+    Title: 'The Matrix',
+    Description:
+      'When a beautiful stranger leads computer hacker Neo to a forbidding underworld, he discovers the shocking truth--the life he knows is the elaborate deception of an evil cyber-intelligence.',
+    ImagePath: 'thematrix.jpg',
+    Director: 'Lana Wachowski',
+    Genre: 'Sci-Fi',
+  },
+  {
+    id: 7,
+    Title: 'The Matrix Reloaded',
+    Description:
+      'Freedom fighters Neo, Trinity and Morpheus continue to lead the revolt against the Machine Army, unleashing their arsenal of extraordinary skills and weaponry against the systematic forces of repression and exploitation.',
+    ImagePath: 'thematrixreloaded.jpg',
+    Director: 'Lana Wachowski',
+    Genre: 'Sci-Fi',
+  },
+  {
+    id: 8,
+    Title: 'The Matrix Revolutions',
+    Description:
+      'The human city of Zion defends itself against the massive invasion of the machines as Neo fights to end the war at another front while also opposing the rogue Agent Smith.',
+    ImagePath: 'thematrixrevolutions.jpg',
+    Director: 'Lana Wachowski',
+    Genre: 'Sci-Fi',
+  },
+  {
+    id: 9,
+    Title: 'Kill Bill: Vol 1',
+    Description:
+      'After awakening from a four-year coma, a former assassin wreaks vengeance on the team of assassins who betrayed her.',
+    ImagePath: 'killbillvol1.jpg',
+    Genre: 'Action',
+    Director: 'Quentin Tarantino',
+  },
+  {
+    id: 10,
+    Title: 'Kill Bill: Vol 2',
+    Description:
+      'The Bride continues her quest of vengeance against her former boss and lover Bill, the reclusive bouncer Budd, and the treacherous, one-eyed Elle.',
+    ImagePath: 'killbillvol2.jpg',
+    Genre: 'Action',
+    Director: 'Quentin Tarantino',
+  },
+];
