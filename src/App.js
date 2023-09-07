48ed38139c5aba94c95145ab60a9c6738695aadd
Terry Roe <terryroe@gmail.com>
2023-09-07 14:01:49 -0600

    import bootstrap

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+import { Fragment } from 'react';
 import './App.css';
 
 function App() {
-  return <div></div>;
+  return (
+    <Fragment>
+      <div className="container">
+        <h1 className="row justify-content-center my-4">MCS Movies</h1>
+      </div>
+    </Fragment>
+  );
 }
 
 export default App;
